08713044e65b5ccd49700ab453a2e2507032b136
MrLister <rlister@wonkasoft.com>
2017-04-13 17:23:11 -0700

    init

---
+++ functions.php
@@ -0,0 +1,115 @@
+ <?php
+/*
+ *  Author: Wonkasoft | @Wonkasoft
+ *  URL: http://wonkasoft.com | 
+ *  Custom functions, support, custom post types and more.
+ */
+ 
+function wonka_targets_setup() {
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed at WordPress.org. See: https://translate.wordpress.org/projects/wp-themes/twentyseventeen
+	 * If you're building a theme based on Twenty Seventeen, use a find and replace
+	 * to change 'twentyseventeen' to the name of your theme in all the template files.
+	 */
+	load_theme_textdomain( 'wonka-targets' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+	add_image_size( 'wonka-targets-featured-image', 2000, 1200, true );
+
+	add_image_size( 'wonka-targets-thumbnail-avatar', 100, 100, true );
+
+	// This theme uses wp_nav_menu() in two locations.
+	register_nav_menus( array(
+		'top'    => __( 'Top Menu', 'wonka-target' ),
+		'cart' => __( 'Cart Menu', 'wonka-target' ),
+	) );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
+
+	/*
+	 * Enable support for Post Formats.
+	 *
+	 * See: https://codex.wordpress.org/Post_Formats
+	 */
+	add_theme_support( 'post-formats', array(
+		'aside',
+		'image',
+		'video',
+		'quote',
+		'link',
+		'gallery',
+		'audio',
+	) );
+
+	// Add theme support for Custom Logo.
+	add_theme_support( 'custom-logo', array(
+		'width'       => 250,
+		'height'      => 250,
+		'flex-width'  => true,
+	) );
+
+	// Add theme support for Custom Logo.
+	add_theme_support( 'custom-cart', array(
+		'width'       => 250,
+		'height'      => 250,
+		'flex-width'  => true,
+	) );
+
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	// Define and register starter content to showcase the theme on new sites.
+	$wonka_start = array(
+		'top' => array(
+			'name' => __('Top Menu','wonka-target'),
+			'items' => array(
+				'link_products',
+				'page_about_us',
+				'page_help',
+				'page_contact_us',
+				),
+			),
+		'cart' => array(
+			'name' => __('Cart Menu','wonka-target'),
+			'items' => array(
+				'link_cart',
+				'page_account',
+				),
+			),
+		);
+	add_theme_support('starter-content', $wonka_start);
+}
+add_action( 'after_setup_theme', 'wonka_targets_setup' );
+ 
+ function wonka_targets_enqueues() {
+ 	wp_enqueue_style( 'style', get_stylesheet_uri());
+ }
+add_action( 'wp_enqueue_scripts', 'wonka_targets_enqueues' );
+ ?>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+<body>
+<div id="w-wrapper">
+		<header id="w-header">
+			<div class="container">
+				<div class="row-fluid">
+					<div class="col-xs-12">
+					<div class="row justify-content-between">
+						<div class="col-xs-3 col-md-3 center">
+							<?php echo get_custom_logo(); ?>
+						</div>
+						<div class="col-xs-3 col-md-3">
+							<ul class="w-cart-menu">
+								<li class="w-menu-item">
+									<a href="#" class="w-links"><span class="w-cart-icon"></span> CART</a>
+								</li>
+								<li class="w-menu-item">
+									<a href="#" class="w-links"><span class="w-menu-option">ACCOUNT</span></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<main>
+			
+
+		</main>
+<?php get_footer(); ?>
