25b19e1bc4ef6327893a555405fce9364497e3c8
MrLister <rlister@wonkasoft.com>
2017-04-14 18:21:00 -0700

    testing

--- index.php
+++ index.php
@@ -1,40 +1,35 @@
 <?php get_header(); ?>
 <body>
-<div id="w-wrapper">
-		<header id="w-header">
-			<div class="container">
-				<div class="row-fluid">
-					<div class="col-xs-12">
-					<div class="row justify-content-between">
-						<div class="col-xs-3 col-md-3 center">
-							<?php echo get_custom_logo(); ?>
-						</div>
-						<div class="col-xs-3 col-md-3">
-							<ul class="w-cart-menu">
-								<li class="w-menu-item">
-									<a href="#" class="w-links"><span class="w-cart-icon"></span> CART</a>
-								</li>
-								<li class="w-menu-item">
-									<a href="#" class="w-links"><span class="w-menu-option">ACCOUNT</span></a>
-								</li>
-							</ul>
-							<?php wp_nav_menu(array(
-								'menu' => 'cart',
-								'menu_class' => 'w-cart-menu',
-								'menu_id' => '',
-								'container' => '',
-								'container_class' => '',
-								'container_id' => '',
-								'fallback_cb' => false,
-								'before' => '\001c',
-							)); ?>
-						</div>
-					</div>
-				</div>
-			</div>
-		</header>
-		<main>
-			
+  <div id="w-wrapper">
+    <header id="w-header">
+      <div class="container">
+        <div class="row-fluid">
+          <div class="col-xs-12">
+            <div class="row justify-content-between">
+              <div class="col-xs-3 col-md-3 center">
+                <?php the_custom_logo();
+                if (!has_custom_logo()) {
+                  ?><a href="<?php esc_url(home_url('/')); ?>">
+                  <img src="<?php echo get_template_directory_uri() . '/assets/images/black-sight-logo.png'; ?>" />
+                  <?php 
+                } ?>
+              </div>
+              <div class="col-xs-3 col-md-3">
+                <ul class="w-cart-menu">
+                  <li class="w-menu-item">
+                    <a href="#" class="w-links"><span class="w-cart-icon"></span> CART</a>
+                  </li>
+                  <li class="w-menu-item">
+                    <a href="#" class="w-links"><span class="w-menu-option">ACCOUNT</span></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main>
 
-		</main>
-<?php get_footer(); ?>
+
+      </main>
+      <?php get_footer(); ?>
